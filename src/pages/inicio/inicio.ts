b3bc96fe1ba9b8fcbb88aba8e13d94af09dd5e40
gorguess <38833997+gorguess@users.noreply.github.com>
2018-05-12 14:42:35 +0200

    primera subida de la APP

---
+++ src/pages/inicio/inicio.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-inicio',
+  templateUrl: 'inicio.html',
+})
+export class InicioPage {
+  items;
+
+  public isSearchbarOpened = false;
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+    // this.listado();
+  }
+
+  listado() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
+
+  // onSearch(text){
+  //   this.listado();
+
+  //   // set val to the value of the text target
+  //   var val = text.target.value;
+
+  //   // if the value is an empty string don't filter the items
+  //   if (val && val.trim() != '') {
+  //     this.items = this.items.filter((item) => {
+  //       return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  //     })
+  //   }
+  // }
+
+  getItems(ev) {
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.listado();
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    } else {
+      return;
+    }
+  }
+
+}
